76dcd683e5ff971c6d6a9d8c90bddac048706cbe
Sunil Kashyap <sunil.kashyap@globallogic.com>
2022-01-05 10:59:34 +0530

    data filter

--- freelancer/src/containers/PorfolioContainer/PortfolioContainer.jsx
+++ freelancer/src/containers/PorfolioContainer/PortfolioContainer.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header, Footer } from '../../components';
 import { basicData } from '../../assets/data/data';
 
 import { useParams } from 'react-router-dom';
 
-function PortfolioContainer() {
+import { portfolioData } from '../../assets/data/data';
 
+function PortfolioContainer() {
   const { slug } = useParams();
+  const [counter, setCounter] = useState(0);
+
+  console.log(' ---- PortfolioContainer ----');
+
+
+  const data = portfolioData.filter((singleData) => {
+    if(singleData.slug == slug){
+      return true;
+    }
+  })[0];
+
+
+  const counterHandler = () => {
+    setCounter(counter + 1);
+  }
 
   return (
     <div className="App">
 
-        <Header  title={basicData.title} />
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-            <h1>Sunil Kashyap -- {slug}</h1>
+        <Header title={basicData.title} />
 
+            <p className='pt-5 mt-5'></p>
+            Counter - {counter}
+            <button onClick={counterHandler}>Increment</button>
+            <h1 className='pt-5 mt-5'>{data.slug}</h1>
+            <img className="img-fluid" src={data.imgSrc} alt={data.imgAlt} />
+            <p>{data.description}</p>
         <Footer />
     </div>
   );
